e9d94237eb987a0ccdc3c356f692103f690843fb
medamshiva20 <medamshiva20@gmail.com>
2026-01-28 13:48:50 +0530

    4.variables.sh

---
+++ 4.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON2:: Hello $PERSON1,How are you"
+echo "$PERSON1:: Ho $PERSON2, I am fine thanks for asking,How are you doing?"
+echo "$PERSON2:: doing fine,What are you learning $PERSON1?"
+echo "$PERSON1:: I am learning shell.What about you"
