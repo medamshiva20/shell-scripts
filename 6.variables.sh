e5aa299a9a7743c8e8e1d4852ba81c53026ff95e
medamshiva20 <medamshiva20@gmail.com>
2026-01-28 14:42:15 +0530

    6.varaiables

--- 6.variables.sh
+++ 6.variables.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
-TIMESTAMP=$(date)
+#TIMESTAMP=$(date)
+#echo "Script executed at :$TIMESTAMP"
 
-echo "Script executed at :$TIMESTAMP"
+START_TIME=$(date +%s)
+
+echo "script executed at:$START_TIME"
+
+sleep 20 
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+
+echo "Script executed in: $TOTAL_TIME seconds"
