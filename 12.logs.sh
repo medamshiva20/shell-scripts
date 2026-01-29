4c02204a74b7e6bea633786caff7087e9a8b0b29
medamshiva20 <medamshiva20@gmail.com>
2026-01-29 16:41:08 +0530

    logs

---
+++ 12.logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOG_FILE="$LOGS_FOLDER/$0.log
+if [ $USERID -ne 0 ] ;
+then
+    echo "Please run this script with root user"
+    exit 1
+fi 
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ;
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+dnf install nodejs -y 
+VALIDATE $? "Installing nodejs"
