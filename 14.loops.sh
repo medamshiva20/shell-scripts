341504a83097559959785878969f74901691d61a
medamshiva20 <medamshiva20@gmail.com>
2026-01-29 18:39:00 +0530

    loops

---
+++ 14.loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR="/var/log/shell-script"
+LOG_FILE="$LOGS_DIR/$0.log"
+
+if [ $USERID -ne 0 ] ;
+then 
+    echo "Please run this script with root user" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ;
+    then 
+        echo "$2...FAILURE" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+
+for package in $@
+do
+ dnf install $package &>>$LOG_FILE
+ VALIDATE $? "$package installation"
+done
