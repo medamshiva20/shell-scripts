10e5a404ed0abab762238df090855d13143e0fe7
medamshiva20 <medamshiva20@gmail.com>
2026-01-29 20:41:06 +0530

    colors

---
+++ 17.colors.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_DIR="/var/log/shell-script"
+LOG_FILE="$LoGS_DIR/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ] ;
+then 
+    echo -e "$R Please run this script with root user $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ] ;
+    then 
+        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+for package in $@
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ] ;
+    then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package already installed ...$Y SKIPPING $N"
+    fi
+done
+
