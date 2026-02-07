6b9225854ba6ab2818c29f4e08602cdbe05fde82
medamshiva20 <medamshiva20@gmail.com>
2026-02-07 12:52:57 +0530

    backup the old logs

---
+++ 23.backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR="/var/log/shell-script/"
+LOG_FILE="$LOGS_DIR/backup.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ] ;
+then 
+    echo "$R Please run this script with root user $N"
+    ecit 1
+fi
+
+USAGE(){
+    echo -e " $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+if [ $USAGE -lt 2 ] ;
+then 
+    USAGE
+fi
