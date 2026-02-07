d2b65a7965aba1f0a1195bbc88154c5317102cda
medamshiva20 <medamshiva20@gmail.com>
2026-02-07 18:02:15 +0530

    backup the old logs

--- 23.backup.sh
+++ 23.backup.sh
@@ -20,18 +20,18 @@ N="\e[0m"
 
 mkdir -p $LOGS_FOLDER
 
-if [ $USERID -ne 0 ] ;
-then 
-    echo "$R Please run this script with root user $N"
-    ecit 1
-fi
-
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
 }
 
+if [ $USERID -ne 0 ] ;
+then 
+    log "$R Please run this script with root user $N"
+    exit 1
+fi
+
 USAGE(){
-    echo -e " $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    log -e " $R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
     exit 1
 }
 
@@ -42,13 +42,13 @@ fi
 
 if [ ! -d $SOURCE_DIR ] ;
 then
-    echo -e "$R Source Directory $SOURCE_DIR does not exist $N"
+    log -e "$R Source Directory $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
 if [ ! -d $DESTINATION_DIR ] ;
 then 
-    echo -e "$R Destination directory $DESTINATION_DIR doesn not exist $N"
+    log -e "$R Destination directory $DESTINATION_DIR doesn not exist $N"
     exit 1
 fi
 
@@ -64,10 +64,10 @@ then
     log "No files to archive ... $Y Skipping $N"
 else
     log "Files found to archieve: $FILES"
-    TIMESTAMP=$(date +%F-%H:%M:%S)
+    TIMESTAMP=$(date +%F-%H-%M-%S)
     ZIP_FILE_NAME="$DESTINATION_DIR/app-logs-$TIMESTAMP.tar.gz"
-    echo "Archieve name:$ZIP_FILE_NAME"
-     tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+        log "Archieve name:$ZIP_FILE_NAME"
+        tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
     #check archieve success or not
     if [ -f $ZIP_FILE_NAME ] ;then
@@ -75,9 +75,9 @@ else
 
         while IFS= read -r filepath
         do 
-            echo Deleting file: $filepath
+            log Deleting file: $filepath
             rm -f $filepath
-            echo Deleted file: $filepath
+            log Deleted file: $filepath
         done <<< $FILE
     else
         log "Archieval is ...$R FAILURE $N"
