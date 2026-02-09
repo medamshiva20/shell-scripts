632e06d6cff26e0df6d5b7ed92ddb1263795ee50
medamshiva20 <medamshiva20@gmail.com>
2026-02-09 14:12:42 +0530

    practicing backup script one more time

--- 24.backup-practice.sh
+++ 24.backup-practice.sh
@@ -27,26 +27,26 @@ USAGE(){
     exit 1
 }
 
-# if [ $# -lt 2 ] ;
-# then 
-#     USAGE
-# fi
+if [ $# -lt 2 ] ;
+then 
+    USAGE
+fi
 
-# if [ ! -d $SOURCE_DIR ] ;
-# then
-#     echo -e "$R Source Directory:$SOURCE_DIR does not exist $N"
-#     exit 1
-# fi
+ if [ ! -d $SOURCE_DIR ] ;
+ then
+     echo -e "$R Source Directory:$SOURCE_DIR does not exist $N"
+     exit 1
+ fi
 
-# if [ ! -d $DEST_DIR ] ;
-# then 
-#     echo -e "$R Destination Directory:$DESI_DIR does not exist $N"
-#     exit 1
-# fi 
+ if [ ! -d $DEST_DIR ] ;
+ then 
+     echo -e "$R Destination Directory:$DESI_DIR does not exist $N"
+     exit 1
+ fi 
 
-# find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS
+ find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS
 
-# log "Backup Started"
-# log "Source Directory:$SOURCE_DIR"
-# log "Destination Directory:$DEST_DIR"
-# log "Days:$DAYS"
+ log "Backup Started"
+ log "Source Directory:$SOURCE_DIR"
+ log "Destination Directory:$DEST_DIR"
+ log "Days:$DAYS"
