a9e7c27a60552d0bc7a1742d79d35b0c5e13493a
medamshiva20 <medamshiva20@gmail.com>
2026-04-08 17:16:15 +0530

    backup scripts

--- 24.backup-practice.sh
+++ 24.backup-practice.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERID = $(id -u)
-LOGS_FOLDER = "/var/log/shell-script"
-LOGS_FILE = "/var/log/shell-script/backup.log"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
 R = "\e[31m"
 G = "\e[32m"
 Y = "\e[33m
