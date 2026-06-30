5b1f40ac5239595511fab52ad934a0c3956cc2c9
medamshiva20 <medamshiva20@gmail.com>
2026-06-30 21:17:27 +0530

    skipping validate command

--- 18.set.sh
+++ 18.set.sh
@@ -17,15 +17,15 @@ fi
 
 mkdir -p $LOGS_DIR
 
-VALIDATE(){
-    if [ $1 -ne 0 ] ;
-    then 
-        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ] ;
+#     then 
+#         echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
+#         exit 1
+#     else
+#         echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+#     fi
+# }
 
 for package in $@
 do
