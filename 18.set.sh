32527936cfa95a3da11cf108be48a52f95acae75
medamshiva20 <medamshiva20@gmail.com>
2026-06-30 21:20:56 +0530

    skipping validate command

--- 18.set.sh
+++ 18.set.sh
@@ -1,42 +1,41 @@
-#!/bin/bash 
+#!/bin/bash
+
+set -e # this will be checking for errors, if errors it will exit
+
+#!/bin/bash
 
 USERID=$(id -u)
-LOGS_DIR="/var/log/shell-script"
-LOG_FILE="$LOGS_DIR/$0.log"
-set -e #This will be checking for errors ,if errors it will exit
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ] ;
-then 
-    echo -e "$R Please run this script with root user $N" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p $LOGS_DIR
+mkdir -p $LOGS_FOLDER
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ] ;
-#     then 
-#         echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
-#         exit 1
-#     else
-#         echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
-#     fi
-# }
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
 
-for package in $@
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
-    dnf list installed $package &>>$LOG_FILE
-    if [ $? -ne 0 ] ;
-    then
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
-        dnf install $package -y &>>$LOG_FILE
+        dnf install $package -y &>>$LOGS_FILE
         #VALIDATE $? "$package installation"
     else
-        echo -e "$package already installed ...$Y SKIPPING $N"
+        echo -e "$package already installed ... $Y SKIPPING $N"
     fi
 done
-
